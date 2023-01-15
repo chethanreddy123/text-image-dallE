ecb30879296a34ee3c756a92419a51eb186260a2
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-01-15 14:52:44 +0530

    added

---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import time
+import os
+import openai
+openai.organization = "org-0ypSMwDVq8KPrWQ2VoclmvC7"
+openai.api_key = "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"
+
+st.sidebar.title('Developer\'s Contact')
+st.sidebar.markdown('[![Chethan-Reddy]'
+                '(https://img.shields.io/badge/Author-Chethan%20Reddy-brightgreen)]'
+                '(https://www.linkedin.com/in/chethan-reddy-0201791ba/)') 
+
+st.sidebar.success("Created Using OpenAI Dall - E")
+
+st.title("Text to image using DALL - E")
+
+text = st.text_input('Enter the Text to generate image: ', 'lion with dog')
+
+check = st.button("Click Here to Generate!")
+
+
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+
+if check == True:
+    response = openai.Image.create(
+    prompt=text,
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    st.image(
+                image_url,
+                width=400, # Manually Adjust the width of the image as per requirement
+            )
+    st.success('Done!')
